134a75d2ce57eacfc2123737ae9e5153fcaad653
yeeun <kye9702@naver.com>
2021-04-03 00:44:37 +0900

    complete

--- src/main.js
+++ src/main.js
@@ -1,2 +1,54 @@
-'use strict';
 
+//Fetch the items from the JSON file
+//fetch를 통해 아이템을 받아온 다음에 받아온 데이터가 성공적이면 제이슨으로 변환하고 제이슨 안에 있는 아이템즈를 리턴하게 된다.
+function loadItems() {
+    return fetch('data/data.json')
+    .then(response => response.json())
+    .then(json => json.items);
+}
+// Update the list with the given items
+function displayItems(items) {
+    const container = document.querySelector('.items');
+    container.innerHTML = items.map(item => createHTMLString(item)).join('');
+}
+
+//Create HTML list item from the given data item
+function createHTMLString(item) {
+    return `
+    <li class="item">
+        <img src=${item.image} alt=${item.type} class="item__thumnail">
+        <span class="item__description">${item.gender}, ${item.size}</span>
+    </li>
+    `;
+}
+
+function onButtonClick(event, items) {
+    // console.log(event.target.dataset.key);
+    // console.log(event.target.dataset.value);
+    const dataset = event.target.dataset;
+    const key = dataset.key;
+    const value = dataset.value;
+
+    if(key ==null || value == null) {
+        return;
+    }
+    // const filtered = items.filter(item => item[key] === value);
+    // console.log(filtered);
+    displayItems(items.filter(item => item[key] === value));
+}
+
+function setEventListeners(items) {
+    const logo = document.querySelector('.logo');
+    const buttons = document.querySelector('.buttons');
+    logo.addEventListener('click', () => displayItems(items));
+    buttons.addEventListener('click', event => onButtonClick(event, items));
+}
+
+//data.json 의 데이터를 읽어와서 데이터를 전달해 주는 역할
+loadItems()
+.then(items => {
+    console.log(items);
+    displayItems(items);
+    setEventListeners(items);
+})
+.catch(console.log);
